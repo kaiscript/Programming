8b74e17ab1f0becd73da5f55592a51839d9f24cd
kaiscript <teardownk@gmail.com>
2016-10-02 12:24:11 +0800

    deleteDuplication

--- src/List/DeleteDuplication.java
+++ src/List/DeleteDuplication.java
@@ -19,6 +19,37 @@ public class DeleteDuplication {
     }
 
     public ListNode deleteDuplication(ListNode pHead) {
+        if (pHead == null)
+            return null;
+        ListNode pre = null; //设置pre前节点，负责连接被删除节点后的节点
+        ListNode node = pHead;
+        while (node != null) {
+            ListNode next = node.next;
+            boolean needDelete = false;
+            if (next != null && next.val == node.val)   //当前节点与下个节点相同，标记为需要删除
+                needDelete = true;
+            if (!needDelete) {   //不需要删除，则标记前驱pre
+                pre = node;
+                node = node.next;
+            } else { //需要删除
+                int delValue = node.val;  //需要删除的节点的值
+                ListNode delNode = node;
+                while (delNode != null && delNode.val == delValue) {  //遍历当前节点及之后的节点，值为需要删除的值将next游标前移
+                    next = delNode.next;
+                    delNode.next = null;
+                    delNode = next;
+                }
+                if (pre == null) //pre为空，则说明头结点是需要被删除的值。将pHead游标移到next
+                    pHead = next;
+                else
+                    pre.next = next; //pre不为空，则将其next指向next节点
 
+                node = next;   //做完操作后。将node游标移到next处
+            }
+
+
+        }
+
+        return pHead;
     }
 }
